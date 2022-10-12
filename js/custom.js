d58665db77e7a45695ebc949d22359b4e182d4f1
Maika Hirata <57153217+uni-mairata@users.noreply.github.com>
2022-10-12 22:45:47 +0000

    First completed version

---
+++ js/custom.js
@@ -0,0 +1,90 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+// carousel 
+
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        420: {
+            items: 2
+        },
+        1000: {
+            items: 5
+        }
+    }
+
+});
+
+// navbar 
+
+var nav = $("#navbarSupportedContent .navbar-nav  ");
+var btn = $(".custom_menu-btn");
+btn.click
+btn.click(function (e) {
+
+    e.preventDefault();
+    nav.toggleClass("lg_nav-toggle");
+    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
+});
+
+// carousel indicators
+$('.carousel').on('slid.bs.carousel', function () {
+    $(".indicator-2 li").removeClass("active");
+    indicators = $(".carousel-indicators li.active").data("slide-to");
+    a = $(".indicator-2").find("[data-slide-to='" + indicators + "']").addClass("active");
+    console.log(indicators);
+
+})
+
+// to toggle nav bar menu
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+// Scroll to top button
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
